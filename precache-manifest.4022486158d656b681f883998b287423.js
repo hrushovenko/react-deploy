bfd82669fa63296c272142e1d07682a4c5d08a3c
pcDimon <dimonvmu@gmail.com>
2020-06-16 13:10:26 +0300

    Updates

---
+++ precache-manifest.4022486158d656b681f883998b287423.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb0f866b4322f3d36494d39c92bef3d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5520b66b435be4e02b28",
+    "url": "/react-deploy/static/css/main.5deeeb0b.chunk.css"
+  },
+  {
+    "revision": "10696fe6f5225d4e3b7f",
+    "url": "/react-deploy/static/js/2.714799cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.714799cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5520b66b435be4e02b28",
+    "url": "/react-deploy/static/js/main.8bf178d2.chunk.js"
+  },
+  {
+    "revision": "325ac91c02c4cc64f93d",
+    "url": "/react-deploy/static/js/runtime-main.ce1c1815.js"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/react-deploy/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "bbacfca0cc4f56ff82a0714317a3eb7f",
+    "url": "/react-deploy/static/media/starEmpty.bbacfca0.png"
+  }
+]);
